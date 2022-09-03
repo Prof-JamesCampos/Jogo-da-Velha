5055764f3fe58e6d3b70b360a90616e1667b8671
Prof-JamesCampos <jwzcampos@gmail.com>
2022-09-02 22:26:07 -0300

    Finalização do projeto

--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.scss'],
 })
-export class SquareComponent implements OnInit {
+export class SquareComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  @Input() value: 'X' | 'O';
 }
